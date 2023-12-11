b8d2564adf8f1c49e3ed394aa18a6c364be65996
Kaluub <kaluubs@gmail.com>
2023-12-11 12:03:31 -0500

    Add temporary message tag.

--- classes/client.js
+++ classes/client.js
@@ -4,17 +4,25 @@ import InteractionHandler from "./interactionHandler.js";
 import NewMemberHandler from "./newMemberHandler.js";
 import Storage from "./postgres.js";
 import RemovedMemberHandler from "./removedMemberHandler.js";
+import MessageHandler from "./messageHandler.js";
 
 class DiscordClient extends Client {
     constructor() {
-        super({ intents: [IntentsBitField.Flags.Guilds, IntentsBitField.Flags.GuildMembers] });
+        super({ intents: [
+            IntentsBitField.Flags.Guilds,
+            IntentsBitField.Flags.GuildMembers,
+            IntentsBitField.Flags.GuildMessages,
+            IntentsBitField.Flags.MessageContent
+        ] });
         this.storage = new Storage();
         this.interactionHandler = new InteractionHandler();
         this.newMemberHandler = new NewMemberHandler();
         this.removedMemberHandler = new RemovedMemberHandler();
+        this.messageHandler = new MessageHandler();
         this.on("interactionCreate", this.interactionHandler.handleInteraction.bind(this.interactionHandler));
         this.on("guildMemberAdd", this.newMemberHandler.handleNewMember.bind(this.newMemberHandler));
         this.on("guildMemberRemove", this.removedMemberHandler.handleRemovedMember.bind(this.removedMemberHandler));
+        this.on("messageCreate", this.messageHandler.handleMessage.bind(this.messageHandler));
     }
 
     async connectStorage() {
---
+++ classes/messageHandler.js
@@ -0,0 +1,19 @@
+import { Message } from "discord.js";
+
+class MessageHandler {
+    /**
+     * Called when a new message is sent.
+     * @param {Message} message 
+     */
+    async handleMessage(message) {
+        if (message.author.bot) return;
+        const messageSplit = message.content.toLowerCase();
+        // TODO: message tags.
+        if (Date.now() > 1702458000000) return;
+        if ((messageSplit.includes("xmas booster") || messageSplit.includes("christmas booster")) && (messageSplit.includes("not available") || messageSplit.includes("not working") || messageSplit.includes("broken") || messageSplit.includes("error"))) {
+            message.reply({content: "⛔ The Xmas booster is not available yet! See an update posted here: https://discord.com/channels/399918427256520705/399964862131994644/1183742241131462697"})
+        }
+    }
+}
+
+export default MessageHandler;
