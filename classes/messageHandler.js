aa76fa359636124302e346980c41074b268ed0b1
Kaluub <kaluubs@gmail.com>
2023-12-11 15:43:21 -0500

    Update specific tags.

--- classes/messageHandler.js
+++ classes/messageHandler.js
@@ -10,7 +10,15 @@ class MessageHandler {
         const messageSplit = message.content.toLowerCase();
         // TODO: message tags.
         if (Date.now() > 1702458000000) return;
-        if ((messageSplit.includes("booster") || messageSplit.includes("xmas")) && (messageSplit.includes("not available") || messageSplit.includes("not working") || messageSplit.includes("broken") || messageSplit.includes("wrong") || messageSplit.includes("error"))) {
+        if ((messageSplit.includes("booster") || messageSplit.includes("xmas")) && (
+            messageSplit.includes("not available")
+            || messageSplit.includes("not working")
+            || messageSplit.includes("broken")
+            || messageSplit.includes("wrong")
+            || messageSplit.includes("open")
+            || messageSplit.includes("not")
+            || messageSplit.includes("buy")
+            || messageSplit.includes("error"))) {
             message.reply({content: "⛔ The Xmas booster is not available yet! See an update posted here: https://discord.com/channels/399918427256520705/399964862131994644/1183742241131462697"})
         }
     }
